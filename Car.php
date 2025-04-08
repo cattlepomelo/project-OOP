297c198f1fb9ea33e33be3effcf2d13532fa3cd7
global <you@example.com>
2025-04-08 14:13:43 +0300

    Constructors in OOP PHP

--- Car.php
+++ Car.php
@@ -5,4 +5,10 @@ class Car {
     private $brand;
     private $color;
     private $vehicleType = "Car";
+
+    // Constructor
+    public function __construct($brand, $color = "none") {
+        $this->brand = $brand;
+        $this->color = $color;
+    }
 }
